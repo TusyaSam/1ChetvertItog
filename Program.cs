1716d95aaa47e2abf7f08cfe37411761bf827c1e
Tatiana Andreeva <tanysha20072@gmail.com>
2022-11-24 13:20:05 +0300

    Итог_

--- Program.cs
+++ Program.cs
@@ -8,5 +8,10 @@ int MaxStringLength = 3;
 
 for (int count = 0; count < ArrayFirst.Length; count++)
 {
-
+    if (ArrayFirst[count].Length <= MaxStringLength)
+    {
+        ArrayItog[count] = ArrayFirst[count];
+    }
+    else ArrayItog[count] = "";
+    Console.Write($"{ArrayItog[count]} ");
 }
