f8592d40e28dee8464dcded797d6b34c5aa09cab
Tatiana Andreeva <tanysha20072@gmail.com>
2022-11-24 12:31:41 +0300

    Цикл прохода всего массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Console.Clear();
+string[] ArrayFirst = new string[] { "one", "two", "three", "four", "fife", "six", "seven", "eight", "nine" };
+string[] ArrayItog = new string[ArrayFirst.Length];
+int MaxStringLength = 3;
+
+for (int count = 0; count < ArrayFirst.Length; count++)
+{}
