f4f36dbd1a3f81cc7ce3e649a040a6b9481511f1
Tatiana Andreeva <tanysha20072@gmail.com>
2022-11-24 13:19:24 +0300

    Вводим данные_

--- Program.cs
+++ Program.cs
@@ -6,12 +6,3 @@ string[] ArrayFirst = new string[] { "one", "two", "three", "four", "fife", "six
 string[] ArrayItog = new string[ArrayFirst.Length];
 int MaxStringLength = 3;
 
-for (int count = 0; count < ArrayFirst.Length; count++)
-{
-    if (ArrayFirst[count].Length <= MaxStringLength)
-    {
-        ArrayItog[count] = ArrayFirst[count];
-    }
-    else ArrayItog[count] = "";
-    Console.Write($"{ArrayItog[count]} ");
-}
